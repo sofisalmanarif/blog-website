a3c4705b35dd281d1ab7ee61fccdb78c575b45c9
sofisalmanarif <sofisalman9906@gmail.com>
2024-10-08 00:31:29 +0530

    react type js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,40 +5,39 @@ import { Button } from "@/components/ui/button";
 import { ButtonIcon } from "@radix-ui/react-icons";
 import { Menu } from "lucide-react";
 import Image from "next/image";
-import { useState } from "react";
+import React, { useState } from "react";
+import Typed from 'typed.js';
+
+
 
 export default function Home() {
   const [isAnnual, setIsAnnual] = useState(true);
+  const el = React.useRef(null);
+ 
+  React.useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ['machine learning','coding', 'web development', 'software development', 'data science', 'artificial intelligence'],
+      typeSpeed: 70,
+    });
+
+    return () => {
+      // Destroy Typed instance during cleanup to stop animation
+      typed.destroy();
+    };
+  }, []);
   return (
     <>
     <main>
         <section className="container px-4 py-10 mx-auto lg:h-128 lg:space-x-8 lg:flex lg:items-center">
       <div className="w-full text-center lg:text-left lg:w-1/2 lg:-mt-8">
         <h1 className="text-3xl leading-snug text-gray-800 dark:text-gray-200 md:text-4xl">
-          A <span className="font-semibold">free repository</span> for community <br className="hidden lg:block" /> components using <span className="font-semibold underline decoration-primary">Tailwind CSS</span>
+          Discover <span className="font-semibold">Inspiring Stories</span> and <br className="hidden lg:block" /> Insights on <span className="font-semibold underline decoration-primary"  ref={el} ></span>
         </h1>
         <p className="mt-4 text-lg text-gray-500 dark:text-gray-300">
-          Open source Tailwind UI components and templates to <br className="hidden lg:block" /> bootstrap your new apps, projects or landing sites!
-        </p>
-        <div className="mt-6 bg-transparent border rounded-lg dark:border-gray-700 lg:w-2/3 focus-within:border-primary focus-within:ring focus-within:ring-primary dark:focus-within:border-primary focus-within:ring-opacity-20">
-          <form action="https://www.creative-tim.com/twcomponents/search" className="flex flex-wrap justify-between md:flex-row">
-            <input
-              type="search"
-              name="query"
-              placeholder="Search Components"
-              required
-              className="flex-1 h-10 px-4 m-1 text-gray-700 placeholder-gray-400 bg-transparent border-none appearance-none lg:h-12 dark:text-gray-200 focus:outline-none focus:placeholder-transparent focus:ring-0"
-            />
-            <button
-              type="submit"
-              className="flex items-center justify-center w-full p-2 m-1 dark:text-black text-white transition-colors duration-300 transform rounded-lg lg:w-12 lg:h-12 lg:p-0 bg-primary hover:bg-primary/70 focus:outline-none focus:bg-primary/70"
-            >
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-              </svg>
-            </button>
-          </form>
-        </div>
+  Stay updated with the latest trends in software development, AI, web development, and machine learning. Explore insightful articles and tutorials to enhance your tech skills and knowledge!
+</p>
+ 
+        
       </div>
       <div className="w-full mt-4 lg:mt-0 lg:w-1/2">
         <img
